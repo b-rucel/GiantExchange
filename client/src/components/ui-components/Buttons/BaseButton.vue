23724dc7c05f9bfbf9c66453ec0414524d1c1c3c
Denis Sel <sfaiross@yandex.ru>
2019-03-18 15:05:12 +0700

    Add Button to storybook

---
+++ client/src/components/ui-components/Buttons/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-button class="style-custom" @click="onClick"><slot></slot></b-button>
+</template>
+
+<script>
+  export default {
+    name: "BaseButton",
+    onClick() {
+      this.$emit('click');
+    }
+  }
+</script>
+
+<style leng="scss" scoped>
+  .style-custom {
+  }
+</style>
--- client/stories/index.stories.js
+++ client/stories/index.stories.js
@@ -1,11 +1,16 @@
 /* eslint-disable react/react-in-jsx-scope, react/no-this-in-sfc */
-
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue);
 import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import MyButton from './MyButton';
 import Welcome from './Welcome';
+import BaseButton from '../src/components/ui-components/Buttons/BaseButton.vue';
 
 storiesOf('Welcome', module).add('to Storybook', () => ({
   components: { Welcome },
@@ -17,6 +22,11 @@ storiesOf('Button', module)
   .add('with text', () => ({
     components: { MyButton },
     template: '<my-button @click="action">Hello Button</my-button>',
+    methods: { action: action('clicked') },
+  }))
+  .add('BaseButton', () => ({
+    components: { BaseButton },
+    template: '<BaseButton @click="action">BaseButton</BaseButton>',
     methods: { action: action('clicked') },
   }))
   .add('with JSX', () => ({
